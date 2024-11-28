8a95cdc194278408ec70f097ae48b26b9f25678d
Mustafa Berk Kalkan <berkklkan@gmail.com>
2024-11-28 20:09:23 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+
+let isActive = true;
+
+function handlePageViewChange() {
+  pageViewsLeft.innerText = (Number(pageViewRange.value) / 16) * 100;
+  if (isActive) {
+    pageViews.innerText = `$ ${Number(pageViewRange.value).toFixed(2)}`;
+    pageViewsMobile.innerText = `$ ${Number(pageViewRange.value).toFixed(2)}`;
+  } else {
+    pageViews.innerText = `$ ${(Number(pageViewRange.value) * 0.75).toFixed(2)}`;
+    pageViewsMobile.innerText = `$ ${(Number(pageViewRange.value) * 0.75).toFixed(2)}`;
+  }
+}
+
+handlePageViewChange();
+
+function changeSliderColor() {
+  const min = pageViewRange.min;
+  const max = pageViewRange.max;
+  const percentage = ((pageViewRange.value - min) / (max - min)) * 100;
+  pageViewRange.style.background = `linear-gradient(to right, #A4F3EB ${percentage}%, #ECF0FB ${percentage}%)`;
+}
+
+switchBtn.addEventListener('click', () => {
+  isActive = !isActive;
+  handlePageViewChange();
+  if (isActive) {
+    switchCircle.style.display = 'block';
+    switchCircleActive.style.display = 'none';
+    switchBtn.style.justifyContent = 'start';
+    switchBtn.style.backgroundColor = '#CFD8EF';
+  } else {
+    switchCircle.style.display = 'none';
+    switchCircleActive.style.display = 'block';
+    switchCircleActive.style.backgroundColor = '#FFFFFF';
+    switchBtn.style.justifyContent = 'end';
+    switchBtn.style.backgroundColor = '#7AEADF';
+  }
+});
+
+pageViewRange.addEventListener('input', handlePageViewChange);
+pageViewRange.addEventListener('input', changeSliderColor);
+
+
